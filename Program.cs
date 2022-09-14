72ae905b0af60038ba787b78cfeeebd6466bc4bc
CleytTheTiger <evgeniy.martyanov.86@gmail.com>
2022-09-14 12:56:41 +0300

    Start of a project

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Lab_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "";
+            double a = 0;
+            double b = 0;
+            double res = 0;
+            str = Console.ReadLine();
+            if (str.Contains("+"))
+            {
+                a = Convert.ToDouble(str.Split("+")[0]);
+                b = Convert.ToDouble(str.Split("+")[1]);
+                res = a + b;
+            }
+            else if (str.Contains("-"))
+            {
+                a = Convert.ToDouble(str.Split("-")[0]);
+                b = Convert.ToDouble(str.Split("-")[1]);
+                res = a - b;
+            }
+            else if (str.Contains("*"))
+            {
+                a = Convert.ToDouble(str.Split("*")[0]);
+                b = Convert.ToDouble(str.Split("*")[1]);
+                res = a * b;
+            }
+            else if (str.Contains("/"))
+            {
+                a = Convert.ToDouble(str.Split("/")[0]);
+                b = Convert.ToDouble(str.Split("/")[1]);
+                res = a / b;
+            }
+            Console.WriteLine(res);
+        }
+    }
+}
